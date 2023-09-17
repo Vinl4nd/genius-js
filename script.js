adc7b0004bbfa3e5007133f4e1a71e2c200895b1
Vinl4nd <139217541+Vinl4nd@users.noreply.github.com>
2023-09-17 12:07:08 +0300

    finish 5th homework

--- script.js
+++ script.js
@@ -179,14 +179,15 @@
 //Замініть цикл "for" на "while"
 
 // for (let i = 0; i < 5; i++) {
-//   console.lpg(`цифра ${i}!`);
+//   console.lщg(`цифра ${i}!`);
 // }
 
 // let i = 0;
 
 // while (i < 5) {
-//   i++;
-//   console.log(i);
+// console.log(i);
+// i++;
+//
 // }
 
 // -- 4 --
@@ -214,3 +215,154 @@
 
 // let summ = result.reduce((sum, result) => sum + result) / result.length;
 // console.log(summ);
+
+// Домашнє завдання. 4
+
+// ---1---
+// Перепишіть функцію, використовуючи '?' або '||'
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('Батьки дозволили?');
+//   }
+// }
+
+// function checkAge(age) {
+//   if (age > 18 || age == 18) {
+//     return true;
+//   } else {
+//     return confirm('Батьки дозволили?');
+//   }
+// }
+
+// checkAge(21);
+
+// ---2---
+// Напишіть функцію min(a, b), яка повертає менше з двох чисел a та b
+
+// const min = function (a, b) {
+//   if (a > b) {
+//     return console.log(a);
+//   } else if (b > a) {
+//     return console.log(b);
+//   }
+// };
+
+// min(18, 9);
+
+// ---3---
+// Перепишіть з використанням стрілкових функцій
+// Замініть Функціональні Вирази на стрілкові функції у коді нижче:
+
+// ask = (question, yes, no) => {
+//   if (confirm(question)) yes();
+//   else no();
+// };
+
+// ask(
+//   'Ви згодні?',
+//   (yes = () => {
+//     alert('Ви погодились.');
+//   }),
+//   (no = () => {
+//     alert('Ви скасували виконання.');
+//   }),
+// );
+
+// Домашнє завдання. 5
+
+// --1--
+// 1) написати об*єкт студента який буде виводити ім*я, спеціальнісь, середній
+// бал і кількість пропущених занять
+// 2) написати метод який буде виводити цю інформацію
+// 3) написати три варіанти студентів
+// 4) прикріпити знначення за допомогою call apply bind
+
+// const showStudentInfo = {
+//   name: 'Evgeniy',
+//   speciality: 'Builder',
+//   avgMark: 4,
+//   missedClass: 0,
+//   showInfo: function () {
+//     console.group(`${this.name} info`);
+//     console.log(`name: ${this.name}`);
+//     console.log(`speciality: ${this.speciality}`);
+//     console.log(`average mark: ${this.avgMark}`);
+//     console.log(`missed class: ${this.missedClass}`);
+//     console.groupEnd();
+//   },
+// };
+
+// const Alexandr = {
+//   name: 'Alex',
+//   speciality: 'Architect',
+//   avgMark: 5,
+//   missedClass: 1,
+// };
+
+// const Nika = {
+//   name: 'Nika',
+//   speciality: 'Designer',
+//   avgMark: 3,
+//   missedClass: 8,
+// };
+
+// showStudentInfo.showInfo();
+
+// showStudentInfo.showInfo.bind(Alexandr)();
+// showStudentInfo.showInfo.bind(Nika)();
+
+// showStudentInfo.showInfo.call(Alexandr);
+// showStudentInfo.showInfo.call(Nika);
+
+// showStudentInfo.showInfo.apply(Alexandr);
+// showStudentInfo.showInfo.apply(Nika);
+
+// --2--
+// Написати дві кнопки і закріпити на них функції
+// при натисканні на кнопку html - має видати коротке визначення що це таке
+// при натисканні на кнопку css - має видати коротке визначення що це таке
+
+// const description = {
+//   showDescription() {
+//     console.log(this.description);
+//   },
+// };
+
+// const CSS = {
+//   description: [
+//     'CSS stands for Cascading Style Sheets. CSS describes how HTML elements are to be displayed on screen, paper, or in other media.',
+//   ],
+// };
+
+// const HTML = {
+//   description: [
+//     'HTML (HyperText Markup Language) is the most basic building block of the Web. It defines the meaning and structure of web content.',
+//   ],
+// };
+
+// document
+//   .querySelector(`#css`)
+//   .addEventListener(`click`, description.showDescription.bind(CSS));
+
+// document
+//   .querySelector(`#html`)
+//   .addEventListener(`click`, description.showDescription.bind(HTML));
+
+// --3--
+// Написати функцію магазин, яка отримує назву товару, ціну за кг і кількість товару
+// функція має повертати назву товару і вартість
+// перевірити на варіантах:
+// 1) banana 30, 4,5
+// 2) cherry 58, 1,3
+// 3) jrange 89. 3,4
+
+// function store() {
+//   item = prompt('введіть назву товару');
+//   price = prompt('введіть ціну за кілограм');
+//   amount = prompt('введіть кількість товару');
+
+//   console.log(alert(`${item} = ${price * amount}`));
+// }
+// store();
